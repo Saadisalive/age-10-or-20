877f0206ba2279417512163db61d48e95f9f81a0
Saadisalive <snafiullah@gmail.com>
2025-04-19 11:06:30 +0500

    Add files via upload

---
+++ age 10 oor 20.py
@@ -0,0 +1,8 @@
+a = int(input("enter age between 10 and 20 :"))
+if (a>=10):
+    if(a<=20):
+        print("the age is valid")
+    else:
+        print("the age is not valid")
+else:
+    print("the age is not valid")        
